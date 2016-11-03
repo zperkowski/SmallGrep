51a4800669993d7c4033ad938e9fd9d87cf5054a
zperkowski <ZdzislawPerkowski@outlook.com>
2016-11-03 20:19:06 +0100

    Checking arguments and opening file

--- sgrep.c
+++ sgrep.c
@@ -3,6 +3,27 @@
 #include <fcntl.h>
 
 int main(int argc, char const *argv[]) {
+  int i, file, patternSize;
+  char const *pattern;
+  int const BUFFOR_SIZE = 256;
+  char buffor[BUFFOR_SIZE];
+
+  // Check how many arguments program has
+  if (argc != 3) {
+    printf("Wrong number of arguments!\n");
+    return(1);
+  }
+
+  // Take pattern
+  pattern = argv[1];
+  patternSize = 0; // TODO Finding pattern size
+
+  // Open file
+  if (file = open(argv[2], O_RDONLY) == -1) {
+    printf("File error!\n");
+    return(3);
+  }
+  // TODO Reading file and looking for the pattern
 
   return(0);
 }
